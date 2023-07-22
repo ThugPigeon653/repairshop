bf77bbaac50534bd91805b5529a11f30b0692c10
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-07-22 22:36:10 +1000

    Issues with cfnresponse
    At time of commit, missing urllib2 is expected to be the issue. Attempted changing lambda to include module logic for direct modification

---
+++ scripts/python/define-tables.py
@@ -0,0 +1,86 @@
+import psycopg2
+import json
+import boto3
+
+
+
+def lambda_handler(event, context):
+    print(event)
+    ssm = boto3.client('ssm')
+    payload = json.loads(event['Payload'])
+    tenant = payload['TENANT']
+    if not tenant:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('TENANT parameter missing in the payload')
+        }
+    sql_statements = sql_statements.replace('TENANT', tenant)
+    try:
+        sql_statements = ssm.get_parameter(Name='/repair/ddl/s3', WithDecryption=False)['Parameter']['Value']
+    except:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('SSM: failed to get /repair/ddl/s3')
+        }
+    try:
+        db_name = ssm.get_parameter(Name='/repair/DBParameter', WithDecryption=False)['Parameter']['Value']
+    except:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('SSM: failed to get /repair/DBParameter')
+        }
+    try:
+        db_user = ssm.get_parameter(Name='/repair/UserParameter', WithDecryption=False)['Parameter']['Value']
+    except:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('SSM: failed to get /repair/UserParameter')
+        }
+    try:
+        db_password = ssm.get_parameter(Name='/repair/PasswordParameter', WithDecryption=False)['Parameter']['Value']
+    except:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('SSM: failed to get PasswordParameter')
+        }
+    try:
+        db_host = ssm.get_parameter(Name='/repair/HostParameter', WithDecryption=False)['Parameter']['Value']
+    except:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('SSM: failed to get /repair/HostParameter')
+        }
+    try:
+        db_port = ssm.get_parameter(Name='/repair/PortParameter', WithDecryption=False)['Parameter']['Value']
+    except:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('SSM: failed to get /repair/PortParameter')
+        }
+
+    try:
+        print("connecting...")
+        conn = psycopg2.connect(
+            dbname=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port
+        )
+        print("connected")
+        cursor = conn.cursor()
+        cursor.execute(sql_statements)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Tables created successfully for tenant: ' + tenant)
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error creating tables: ' + str(e))
+        }
--- scripts/sql/ddl.sql
+++ scripts/sql/ddl.sql
@@ -6,7 +6,7 @@
 -- CLOUDFORMAION MUST DO:
 --  - create parameter store for db creds
 
-CREATE TABLE public.customers (
+CREATE TABLE public.customers_TENANT (
     customer_id serial PRIMARY KEY,
     first_name text CHECK (first_name ~ '^[A-Za-z]{0,25}$') NOT NULL,
     last_name text CHECK (last_name ~ '^[A-Za-z]{0,25}$') NOT NULL,
@@ -17,10 +17,10 @@ CREATE TABLE public.customers (
     address_line_2 text,
     city text,
     state_territory text,
-    zip_code integer CHECK (zip_code ~ '^\d{4}$'),
+    zip_code integer CHECK (zip_code >= 1000 AND zip_code <= 9999),
     country text CHECK (country ~ '^[^0-9]*$'),
-    referred_by integer REFERENCES customers (customer_id),
-    tax_rate CHECK (tax_rate ~ '^\d{1,2}(\.\d+)?$'),
+    referred_by integer REFERENCES public.customers_TENANT (customer_id),
+    tax_rate numeric CHECK (tax_rate >= 0.00 AND tax_rate <=99.99),
     is_sms boolean,
     is_billing_emails boolean,
     is_marketing_emails boolean,
@@ -30,15 +30,15 @@ CREATE TABLE public.customers (
     invoice_cc_email text CHECK (invoice_cc_email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')
 );
 
-CREATE TABLE public.stores (
+CREATE TABLE public.stores_TENANT (
     store_id serial PRIMARY KEY,
     address_line_1 text,
     city text,
-    zip_code integer CHECK (zip_code ~ '^\d{4}$'),
+    zip_code integer CHECK (zip_code >= 1000 AND zip_code <= 9999),
     phone text CHECK (phone ~* '^(\+61|0)4\d{8}$|^\d{8}$')
 );
 
-CREATE TABLE public.items (
+CREATE TABLE public.items_TENANT (
     item_id serial PRIMARY KEY,
     item_display_name text,
     item_make text,
@@ -49,7 +49,7 @@ CREATE TABLE public.items (
     custom_fields jsonb
 );
 
-CREATE TABLE public.assets (
+CREATE TABLE public.assets_TENANT (
     asset_id serial PRIMARY KEY,
     asset_name text NOT NULL,
     asset_serial text,
@@ -59,44 +59,44 @@ CREATE TABLE public.assets (
     notes text
 );
 
-CREATE TABLE public.stock (
+CREATE TABLE public.stock_TENANT (
     barcode text CHECK (barcode ~ '^.{2,43}$'),
     net_paid decimal NOT NULL,
     tax_paid decimal NOT NULL,
     net_sale_price decimal NOT NULL,
     tax_charged decimal NOT NULL,
-    item integer REFERENCES public.items (item_id)
+    item integer REFERENCES public.items_TENANT (item_id)
 );
 
-CREATE TABLE public.employees (
+CREATE TABLE public.employees_TENANT (
     employee_id serial PRIMARY KEY,
     first_name text NOT NULL CHECK (first_name ~ '^[A-Za-z]{0,25}$'),
     last_name text NOT NULL CHECK (last_name ~ '^[A-Za-z]{0,25}$'),
     email text CHECK (email ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
-    store integer REFERENCES stores (store_id)
+    store integer REFERENCES public.stores_TENANT (store_id)
 );
 
-CREATE TABLE public.tickets (
+CREATE TABLE public.tickets_TENANT (
     ticket_id serial PRIMARY KEY,
-    customer_id integer REFERENCES customers (customer_id) NOT NULL,
+    customer_id integer REFERENCES public.customers_TENANT (customer_id) NOT NULL,
     ticket_title text NOT NULL,
     ticket_priority_level integer NOT NULL,
     ticket_description text NOT NULL,
     creation_date date NOT NULL,
     due_date date NOT NULL,
-    tech integer REFERENCES employees (employee_id),
+    tech integer REFERENCES public.employees_TENANT (employee_id),
     net_price decimal NOT NULL,
     gross_price decimal
 );
 
-CREATE TABLE public.invoices (
+CREATE TABLE public.invoices_TENANT (
     invoice_id serial PRIMARY KEY,
     invoice_status boolean NOT NULL,
     date_created date NOT NULL,
     is_taxable boolean NOT NULL,
     date_paid date,
     tech_notes text,
-    ticket integer REFERENCES tickets (ticket_id),
+    ticket integer REFERENCES public.tickets_TENANT (ticket_id),
     payment_method text,
     purchase_order_number integer
 );
